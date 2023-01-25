211226cfc2dd4ec98a4f492d37c5eb1ee8efca25
nutjunkie <andrew.iqmol@gmail.com>
2023-01-25 07:29:22 +0000

    windows changes

--- src/Main/version.h
+++ src/Main/version.h
@@ -1,5 +1,5 @@
 // Version file generated on "Wed, Jul 14, 2021  9:59:39 PM"
 // Automatically generated file, do not edit
 #define IQMOL_YEAR "2022"
-#define IQMOL_VERSION "3.0.0"
+#define IQMOL_VERSION "3.0.1"
 #define GIT_VERSION "2.15.0-15-gefc7a27"
