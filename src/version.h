e1b6368545e1067dbd5d30b97911d80b41508be4
Andrew Gilbert <andrew.gilbert@anu.edu.au>
2023-02-09 16:15:03 +1100

    Tagged v3.1.2

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-// Version file generated on "Tue 31 Jan 2023 11:26:43 AEDT"
+// Version file generated on "Thu  9 Feb 2023 16:14:04 AEDT"
 // Automatically generated file, do not edit
 #define IQMOL_YEAR "2023"
-#define IQMOL_VERSION "3.1.1"
-#define GIT_VERSION "3.1.1-1-gad9b5e1"
+#define IQMOL_VERSION "v3.1.2"
+#define GIT_VERSION "v3.1.2"
