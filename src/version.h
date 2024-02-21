cfc70b636d91d912821ecfbc0c29a8da78e671b6
Andrew Gilbert <andrew.gilbert@anu.edu.au>
2024-02-21 21:57:00 +1100

    Version update

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
-// Version file generated on "Wed 21 Feb 2024 14:44:11 AEDT"
+// Version file generated on "Wed 21 Feb 2024 21:56:16 AEDT"
 // Automatically generated file, do not edit
 #define IQMOL_YEAR "2024"
-#define IQMOL_VERSION "v3.1.2"
-#define GIT_VERSION "v3.1.2-29-g9aa569a"
+#define IQMOL_VERSION "v3.1.3"
+#define GIT_VERSION "v3.1.3"
